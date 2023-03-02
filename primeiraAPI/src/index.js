d5eccd6e4edd6bc52fe6302726ff41262d31bd2b
liviapires <livia.onofre@fatec.sp.gov.br>
2023-03-02 13:50:17 -0300

    Primeiras configurações para construção da API

--- primeiraAPI/src/index.js
+++ primeiraAPI/src/index.js
@@ -3,16 +3,47 @@ const express = require('express');
 
 const app = express();
 
-app.get('/texto', (request, response) => {
-    return response.json(":'D");
+app.get('/products', (request, response) => {
+    return response.json(
+        [{
+            "id": 1,
+            "name": "Caderno Pequeno",
+            "image": "cadernopequeno.jpg",
+            "category": "Papelaria",
+            "description": "Caderno pequeno de 40 folhas",
+            "status": "Disponível",
+        },
+        {
+            "id": 2,
+            "name": "Caderno Grande",
+            "image": "cadernogrande.jpg",
+            "category": "Papelaria",
+            "description": "Caderno grande de 100 folhas",
+            "status": "Disponível",
+        },
+    ]
+    );
     }
 );
 
-app.get('/json', (request, response) => {
-    return response.json({
-        "nome": "João",
-        "idade": 20
-    });
+app.post('/products', (request, response) => {
+    const body = request.body;
+    console.log(body);
+    const {name, image, category, description, status} = request.body;
+    min = Math.ceil(0);
+    max = Math.floor(99);
+    const id = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    let object = {
+        id,
+        name,
+        image,
+        category,
+        description,
+        status
+    }
+
+    return response.json(object);
 });
 
 app.listen(3000);
