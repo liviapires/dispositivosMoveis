d89707a375e543c7e8c5f044a2327c0066087da2
Vini <vinicius.sugawara@gmail.com>
2023-04-13 02:32:04 -0300

    fix: made products controllers use services

--- primeiraAPI/src/controllers/productControllers/DeleteProductController.ts
+++ primeiraAPI/src/controllers/productControllers/DeleteProductController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
+import { DeleteProductService } from "../../services/productsServices/DeleteProductService";
 
 class DeleteProductController {
-    async handle(request: Request, response: Response){
-        return response.json("Deleted Resource");
-    }
+    async handle(request: Request, response: Response) {
+    
+        const id= request.params.id;
+        const deleteProductService= new DeleteProductService();
+    
+       const msg = await deleteProductService.execute({
+         id
+       });
+    
+       return response.json(msg);
+    
+      }
 }
 
 export { DeleteProductController };
--- primeiraAPI/src/controllers/productControllers/ListProductController.ts
+++ primeiraAPI/src/controllers/productControllers/ListProductController.ts
@@ -1,30 +1,11 @@
 import { Request, Response } from 'express';
+import { ListProductService } from '../../services/productsServices/ListProductService';
 
 class ListProductController{
     async handle(request: Request, response: Response){
-        const products = [
-            { 
-                "id": "1",
-                "name": "Apple",
-                "description": "Red Fruit",
-                "price": "0.25",
-                "url": "www.apple.com"
-            },
-            { 
-                "id": "2",
-                "name": "Orange",
-                "description": "Orange Fruit",
-                "price": "0.50",
-                "url": "www.orange.com"
-            },
-            { 
-                "id": "3",
-                "name": "Watermelon",
-                "description": "Green outside, red inside Fruit",
-                "price": "5.00",
-                "url": "www.watermelon.com"
-            }
-        ];
+        const listProductsService = new ListProductService();
+
+        const products = await listProductsService.execute();
 
         return response.json(products);
     }
