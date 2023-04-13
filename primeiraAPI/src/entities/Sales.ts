9a5789097bba2221be4c62f6b2aeccd8c59248d5
liviapires <liviapso2012@gmail.com>
2023-04-12 22:35:48 -0300

    Feat: Add Sales entity

---
+++ primeiraAPI/src/entities/Sales.ts
@@ -0,0 +1,37 @@
+import {
+    Entity,
+    PrimaryColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from "typeorm";
+  import { v4 as uuid } from "uuid";
+  
+  @Entity("sales")
+  class Sale {
+    @PrimaryColumn()
+    readonly id!: string ;
+  
+    @Column()
+      total!: string;
+  
+    @Column()
+      description!: string;
+  
+    @Column()
+      observarion!: string;
+  
+    @CreateDateColumn()
+      created_at!: Date;
+  
+    @UpdateDateColumn()
+      updated_at!: Date;
+  
+    constructor() {
+      if (!this.id) {
+        this.id = uuid();
+      }
+    }
+  }
+  
+  export { Sale };
